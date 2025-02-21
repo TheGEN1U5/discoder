08486bb7c3a267acc3a4cb0e54515265219ddc21
Pranav Arora <pranavjarora1@gmail.com>
2025-02-21 19:30:55 +0530

    Update workflows.py
    summary returned in string format

--- workflows.py
+++ workflows.py
@@ -1,15 +1,16 @@
-from mira_sdk import MiraClient, Flow
+from mira_sdk import MiraClient, Flow, File
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 client = MiraClient(config={"API_KEY": os.getenv("MIRA_API_KEY")})
 
-def discussion_summariser(discussion):
+def discussion_summariser(discussion=''):
     summarizer_flow = Flow(source="flows/summarizer-flow.yaml")
     input_dict = {"discussion" : discussion}
     response = client.flow.test(summarizer_flow, input_dict)
-    return str(response)
+
+    return response['result']
 
 async def readme_summariser(readme):
     return readme
